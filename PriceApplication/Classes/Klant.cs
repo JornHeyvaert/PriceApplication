a4a8369166a7db93e090543c7ddb0b51a5a1b664
Jorn Heyvaert <jorn.heyvaert@telenet.be>
2017-04-10 21:33:44 +0200

    Renewing Application

---
+++ PriceApplication/Classes/Klant.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceApplication.Classes
+{
+    public class Klant : INotifyPropertyChanged
+    {
+        #region Variables
+
+        private string _name;
+
+        #endregion
+
+        #region Properties
+
+        public int KlantID { get; set; }
+        public string KlantNaam
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                onPropertyChanged(this, "KlantNaam");
+            }
+        }
+        public int Aantal100Consumpties { get; set; }
+        public int Aantal150Consumpties { get; set; }
+        public int Aantal200Consumpties { get; set; }
+        public int Aantal250Consumpties { get; set; }
+        public double Totaleprijs { get; set; }
+        public double TotaleprijsDividedByTwo { get; set; }
+        public double TotaleprijsDividedByThree { get; set; }
+        public double TotaleprijsDividedByFour { get; set; }
+        public double PrijsVanPersoonDieWegIs { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Methods
+
+        private void onPropertyChanged(object sender, string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(sender, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
--- PriceApplication/MainWindow.xaml.cs
+++ PriceApplication/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PriceApplication.Classes;
+using System;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PriceApplication
 {
@@ -20,116 +12,268 @@ namespace PriceApplication
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int aantal100Consumpties;
-        private int aantal150Consumpties;
-        private int aantal200Consumpties;
-        private int aantal250Consumpties;
-
-        private double totalePrijs;
+        private ObservableCollection<Klant> klanten;
+        private Klant geselecteerdeKlant;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        #region Events
+
         private void Window_Initialized(object sender, EventArgs e)
         {
             Initialization();
         }
 
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            EnableOrDisableButtons(klanten.Count > 0);
+        }
+
+        private void btnAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtCustomerName.Text))
+            {
+                Klant klant = new Klant()
+                {
+                    KlantNaam = (bool)chkIsPloeg.IsChecked ? $"{txtCustomerName.Text} ({txtAantalAanwezig.Text} personen aanwezig)" : $"{txtCustomerName.Text}",
+                    KlantID = klanten.Count + 1
+                };
+                klanten.Add(klant);
+                geselecteerdeKlant = klanten.OrderBy(x => x.KlantID).Last();
+                DisplayPrices();
+                listCustomers.ItemsSource = klanten;
+                txtCustomerName.Text = string.Empty;
+            }
+            EnableOrDisableButtons(klanten.Count > 0);
+        }
+
         private void btnIncreasePrice150_Click(object sender, RoutedEventArgs e)
         {
-            aantal150Consumpties++;
-            txtPrice150.Text = aantal150Consumpties.ToString();
+            geselecteerdeKlant.Aantal150Consumpties++;
+            txtPrice150.Text = geselecteerdeKlant.Aantal150Consumpties.ToString();
             CalculateTotalPrice();
         }
 
         private void btnDecreasePrice150_Click(object sender, RoutedEventArgs e)
         {
-            if( aantal150Consumpties > 0) { aantal150Consumpties--; }
-            txtPrice150.Text = aantal150Consumpties.ToString();
+            if (geselecteerdeKlant.Aantal150Consumpties > 0)
+            {
+                geselecteerdeKlant.Aantal150Consumpties--;
+            }
+            txtPrice150.Text = geselecteerdeKlant.Aantal150Consumpties.ToString();
             CalculateTotalPrice();
         }
 
         private void btnIncreasePrice200_Click(object sender, RoutedEventArgs e)
         {
-            aantal200Consumpties++;
-            txtPrice200.Text = aantal200Consumpties.ToString();
+            geselecteerdeKlant.Aantal200Consumpties++;
+            txtPrice200.Text = geselecteerdeKlant.Aantal200Consumpties.ToString();
             CalculateTotalPrice();
         }
 
         private void btnDecreasePrice200_Click(object sender, RoutedEventArgs e)
         {
-            if (aantal200Consumpties > 0) { aantal200Consumpties--; } 
-            txtPrice200.Text = aantal200Consumpties.ToString();
+            if (geselecteerdeKlant.Aantal200Consumpties > 0)
+            {
+                geselecteerdeKlant.Aantal200Consumpties--;
+            }
+            txtPrice200.Text = geselecteerdeKlant.Aantal200Consumpties.ToString();
             CalculateTotalPrice();
         }
 
         private void btnIncreasePrice250_Click(object sender, RoutedEventArgs e)
         {
-            aantal250Consumpties++; 
-            txtPrice250.Text = aantal250Consumpties.ToString();
+            geselecteerdeKlant.Aantal250Consumpties++;
+            txtPrice250.Text = geselecteerdeKlant.Aantal250Consumpties.ToString();
             CalculateTotalPrice();
         }
 
         private void btnDecreasePrice250_Click(object sender, RoutedEventArgs e)
         {
-            if (aantal250Consumpties > 0) { aantal250Consumpties--; }
-            txtPrice250.Text = aantal250Consumpties.ToString();
+            if (geselecteerdeKlant.Aantal250Consumpties > 0)
+            {
+                geselecteerdeKlant.Aantal250Consumpties--;
+            }
+            txtPrice250.Text = geselecteerdeKlant.Aantal250Consumpties.ToString();
             CalculateTotalPrice();
         }
 
         private void btnIncreasePrice100_Click(object sender, RoutedEventArgs e)
         {
-            aantal100Consumpties++; 
-            txtPrice100.Text = aantal100Consumpties.ToString();
+            geselecteerdeKlant.Aantal100Consumpties++;
+            txtPrice100.Text = geselecteerdeKlant.Aantal100Consumpties.ToString();
             CalculateTotalPrice();
         }
 
         private void btnDecreasePrice100_Click(object sender, RoutedEventArgs e)
         {
-            if (aantal100Consumpties > 0) { aantal100Consumpties--; }
-            txtPrice100.Text = aantal100Consumpties.ToString();
+            if (geselecteerdeKlant.Aantal100Consumpties > 0)
+            {
+                geselecteerdeKlant.Aantal100Consumpties--;
+            }
+            txtPrice100.Text = geselecteerdeKlant.Aantal100Consumpties.ToString();
             CalculateTotalPrice();
         }
 
         private void btnAfrekenen_Click(object sender, RoutedEventArgs e)
         {
-            PaymentDone();
+            klanten.Remove(geselecteerdeKlant);
+            RecalculateKlantIDs();
+            if (klanten.Count > 0)
+            {
+                geselecteerdeKlant = klanten.OrderBy(x => x.KlantID).First();
+                DisplayPrices();
+            }
+            else
+            {
+                Reset();
+            }
+            listCustomers.ItemsSource = klanten;
+            EnableOrDisableButtons(klanten.Count > 0);
         }
+
+        private void listCustomers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listCustomers.SelectedIndex != -1)
+            {
+                geselecteerdeKlant = klanten.Where(x => x.KlantID == listCustomers.SelectedIndex + 1).First();
+                DisplayPrices();
+            }
+        }
+
+        private void btnEnkelvoudigeAfrekeningTweePersonen_Click(object sender, RoutedEventArgs e)
+        {
+            EnkelvoudigeAfrekening(2);
+            listCustomers.ItemsSource = klanten;
+            lblCurrentCustomerName.Content = geselecteerdeKlant.KlantNaam;
+        }
+
+        private void btnEnkelvoudigeAfrekeningDriePersonen_Click(object sender, RoutedEventArgs e)
+        {
+            EnkelvoudigeAfrekening(3);
+            listCustomers.ItemsSource = klanten;
+            lblCurrentCustomerName.Content = geselecteerdeKlant.KlantNaam;
+        }
+
+        private void btnEnkelvoudigeAfrekeningVierPersonen_Click(object sender, RoutedEventArgs e)
+        {
+            EnkelvoudigeAfrekening(4);
+            lblCurrentCustomerName.Content = geselecteerdeKlant.KlantNaam;
+            listCustomers.ItemsSource = klanten;
+        }
+
+        private void chkIsPloeg_Checked(object sender, RoutedEventArgs e)
+        {
+            txtAantalAanwezig.IsEnabled = true;
+        }
+
+        private void chkIsPloeg_Unchecked(object sender, RoutedEventArgs e)
+        {
+            txtAantalAanwezig.Text = string.Empty;
+            txtAantalAanwezig.IsEnabled = false;
+        }
+
+        #endregion Events
 
         #region Helper Methods
 
-        public void CalculateTotalPrice()
+        private void EnkelvoudigeAfrekening(int aantalPersonenAanwezig)
         {
-            totalePrijs = (aantal100Consumpties * 1) + (aantal150Consumpties * 1.5) + (aantal200Consumpties * 2) + (aantal250Consumpties * 2.5);
-            lblTotalePrijsAmount.Content = string.Format("€ {0}", totalePrijs.ToString());
+            if (aantalPersonenAanwezig == 2)
+            {
+                geselecteerdeKlant.Totaleprijs = geselecteerdeKlant.Totaleprijs - geselecteerdeKlant.TotaleprijsDividedByTwo;
+                geselecteerdeKlant.PrijsVanPersoonDieWegIs += geselecteerdeKlant.TotaleprijsDividedByTwo;
+                geselecteerdeKlant.KlantNaam = geselecteerdeKlant.KlantNaam.Contains("aanwezig") ? geselecteerdeKlant.KlantNaam.Replace('2','1') : geselecteerdeKlant.KlantNaam;
+            }
+            else if (aantalPersonenAanwezig == 3)
+            {
+                geselecteerdeKlant.Totaleprijs = geselecteerdeKlant.Totaleprijs - geselecteerdeKlant.TotaleprijsDividedByThree;
+                geselecteerdeKlant.PrijsVanPersoonDieWegIs += geselecteerdeKlant.TotaleprijsDividedByThree;
+                geselecteerdeKlant.KlantNaam = geselecteerdeKlant.KlantNaam.Contains("aanwezig") ? geselecteerdeKlant.KlantNaam.Replace('3', '2') : geselecteerdeKlant.KlantNaam;
+            }
+            else
+            {
+                geselecteerdeKlant.Totaleprijs = geselecteerdeKlant.Totaleprijs - geselecteerdeKlant.TotaleprijsDividedByFour;
+                geselecteerdeKlant.PrijsVanPersoonDieWegIs += geselecteerdeKlant.TotaleprijsDividedByFour;
+                geselecteerdeKlant.KlantNaam = geselecteerdeKlant.KlantNaam.Contains("aanwezig") ? geselecteerdeKlant.KlantNaam.Replace('4', '3') : geselecteerdeKlant.KlantNaam;
+            }
+            CalculateCustomPrices();
+        }
+
+        private void EnableOrDisableButtons(bool isEnabled)
+        {
+            btnAfrekenen.IsEnabled = isEnabled;
+            btnDecreasePrice100.IsEnabled = isEnabled;
+            btnDecreasePrice150.IsEnabled = isEnabled;
+            btnDecreasePrice200.IsEnabled = isEnabled;
+            btnDecreasePrice250.IsEnabled = isEnabled;
+            btnIncreasePrice100.IsEnabled = isEnabled;
+            btnIncreasePrice150.IsEnabled = isEnabled;
+            btnIncreasePrice200.IsEnabled = isEnabled;
+            btnIncreasePrice250.IsEnabled = isEnabled;
+            btnEnkelvoudigeAfrekeningTweePersonen.IsEnabled = isEnabled;
+            btnEnkelvoudigeAfrekeningDriePersonen.IsEnabled = isEnabled;
+            btnEnkelvoudigeAfrekeningVierPersonen.IsEnabled = isEnabled;
+        }
+
+        private void Reset()
+        {
+            txtPrice100.Text = string.Empty;
+            txtPrice150.Text = string.Empty;
+            txtPrice200.Text = string.Empty;
+            txtPrice250.Text = string.Empty;
+            lblTotalePrijsAmount.Content = string.Empty;
+            lblTotalePrijsAmountDividedByTwo.Content = string.Empty;
+            lblTotalePrijsAmountDividedByThree.Content = string.Empty;
+            lblTotalePrijsAmountDividedyFour.Content = string.Empty;
+            lblCurrentCustomerName.Content = string.Empty;
+        }
+
+        private void RecalculateKlantIDs()
+        {
+            for (int i = 0; i < klanten.Count; i++)
+            {
+                klanten[i].KlantID = i + 1;
+            }
+        }
+
+        private void CalculateTotalPrice()
+        {
+            geselecteerdeKlant.Totaleprijs = (geselecteerdeKlant.Aantal100Consumpties * 1) + (geselecteerdeKlant.Aantal150Consumpties * 1.5)
+                                             + (geselecteerdeKlant.Aantal200Consumpties * 2) + (geselecteerdeKlant.Aantal250Consumpties * 2.5) - geselecteerdeKlant.PrijsVanPersoonDieWegIs;
+            CalculateCustomPrices();
+        }
+
+        private void CalculateCustomPrices()
+        {
+            geselecteerdeKlant.TotaleprijsDividedByTwo = geselecteerdeKlant.Totaleprijs / 2;
+            geselecteerdeKlant.TotaleprijsDividedByThree = geselecteerdeKlant.Totaleprijs / 3;
+            geselecteerdeKlant.TotaleprijsDividedByFour = geselecteerdeKlant.Totaleprijs / 4;
+            lblTotalePrijsAmount.Content = string.Format("€ {0}", Math.Round(geselecteerdeKlant.Totaleprijs,2));
+            lblTotalePrijsAmountDividedByTwo.Content = string.Format("€ {0}", Math.Round(geselecteerdeKlant.TotaleprijsDividedByTwo,2));
+            lblTotalePrijsAmountDividedByThree.Content = string.Format("€ {0}", Math.Round(geselecteerdeKlant.TotaleprijsDividedByThree,2));
+            lblTotalePrijsAmountDividedyFour.Content = string.Format("€ {0}", Math.Round(geselecteerdeKlant.TotaleprijsDividedByFour,2));
         }
 
         private void Initialization()
         {
-            txtPrice100.Text = aantal100Consumpties.ToString();
-            txtPrice150.Text = aantal150Consumpties.ToString();
-            txtPrice200.Text = aantal200Consumpties.ToString();
-            txtPrice250.Text = aantal250Consumpties.ToString();
+            klanten = new ObservableCollection<Klant>();
+            listCustomers.ItemsSource = klanten;
         }
 
-        public void Reset()
+        private void DisplayPrices()
         {
-            aantal100Consumpties = 0;
-            aantal150Consumpties = 0;
-            aantal200Consumpties = 0;
-            aantal250Consumpties = 0;
-            totalePrijs = 0;
-            lblTotalePrijsAmount.Content = string.Empty;
+            lblCurrentCustomerName.Content = geselecteerdeKlant.KlantNaam;
+            txtPrice100.Text = geselecteerdeKlant.Aantal150Consumpties.ToString();
+            txtPrice150.Text = geselecteerdeKlant.Aantal150Consumpties.ToString();
+            txtPrice200.Text = geselecteerdeKlant.Aantal200Consumpties.ToString();
+            txtPrice250.Text = geselecteerdeKlant.Aantal250Consumpties.ToString();
+            CalculateTotalPrice();
         }
 
-        public void PaymentDone()
-        {
-            Reset();
-            Initialization();
-        }
-
-        #endregion
+        #endregion Helper Methods
     }
 }
